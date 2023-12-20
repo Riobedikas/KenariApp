f241e1b8d9c4efef12cb8b59ea327570d35fac76
Hii <a193bsy2444@bangkit.academy>
2023-12-21 01:57:30 +0700

    Edit Layout Discussion Forum

--- app/src/main/java/com/dicoding/kenari/view/chat/ChatActivity.kt
+++ app/src/main/java/com/dicoding/kenari/view/chat/ChatActivity.kt
@@ -1,15 +1,30 @@
 package com.dicoding.kenari.view.chat
 
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dicoding.kenari.R
+import com.dicoding.kenari.databinding.ActivityChatBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ChatActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityChatBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+
+        val newDiscussion = findViewById<FloatingActionButton>(R.id.btnAdd)
+        newDiscussion.setOnClickListener {
+            val intent = Intent(this, AddDiscussionActivity::class.java)
+            startActivity(intent)
+        }
+        /*binding.btnAdd.setOnClickListener {
+            val intent = Intent(this, AddDiscussionActivity::class.java)
+            startActivity(intent)
+        }*/
 
         supportActionBar?.title = "Chat"
         supportActionBar?.setBackgroundDrawable(ColorDrawable(Color.parseColor("#20BAB3")))
